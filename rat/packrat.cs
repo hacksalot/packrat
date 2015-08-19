d6ea454b23d502a92655ba4fd4db28773c0b8984
gruebait <gruebait@eatenbygrues.com>
2015-08-18 20:45:06 -0400

    Introduce atlas-friendly mipmap gen.
    Add preliminary atlas-friendly mipmap generation. Instead of
    implementing periodic downsampling of the atlas itself via FFT or
    another means, downsample each tile directly prior to packing.

--- rat/packrat.cs
+++ rat/packrat.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Linq;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Collections.Generic;
 
 namespace prc {
@@ -36,7 +37,7 @@ namespace prc {
 
       // Save and we're done
       atlas.save( atlas.file );
-      return atlas.raw;
+      return atlas.mips[0];
     }
 
     // Load a source image file
@@ -53,24 +54,71 @@ namespace prc {
 
     // Process a source image file
     prImg proc( prImg org ) {
+      if( _genMips ) {
+        if (org.mips == null)
+          org.mips = new List<Image>();
+        for( int mipLevel = 1, // a modest for loop
+             f = (int)Math.Pow(2,mipLevel),
+             w = org.mips[0].Width / f,
+             h = org.mips[0].Height / f;
+             w != 0 && h != 0;
+             mipLevel++,
+             f = (int) Math.Pow(2, mipLevel),
+             w = org.mips[0].Width / f,
+             h = org.mips[0].Height / f ) {
+          Bitmap mipImg = new Bitmap( w, h );
+          Graphics gtemp = Graphics.FromImage( mipImg );
+          gtemp.InterpolationMode = _mode;
+          gtemp.DrawImage( org.mips[0], new Rectangle( 0, 0, w, h ) );
+          gtemp.Dispose();
+          org.mips.Add( mipImg );
+        }
+      }
       if( Processed != null )
         Processed( this, new ImageEventArgs( org.id, org.file ));
       return org;
     }
 
+    // Generate empty mipmaps for the image
+    void prepMips( prImg img ) {
+      for (int mipLevel = 1, // mega for-loop
+           f = (int) Math.Pow( 2, mipLevel ),
+           w = img.mips[0].Width / f,
+           h = img.mips[0].Height / f;
+           w != 0 && h != 0;
+           mipLevel++,
+           f = (int) Math.Pow( 2, mipLevel ),
+           w = img.mips[0].Width / f,
+           h = img.mips[0].Height / f) {
+        Bitmap mipImg = new Bitmap( w, h );
+        img.mips.Add( mipImg );
+      }
+    }
+
     // Prepare the atlas
     prImg prep( string file ) {
-      var img = new Bitmap( Math.Min(_count, _xu) * _texSize.Width, 
+      var img = new Bitmap( Math.Min(_count, _xu) * _texSize.Width,
                             (1 + (_count / _xu)) * _texSize.Height );
-      _g = Graphics.FromImage( img );
-      return new prImg( file, img, -1 );
+      var pimg = new prImg( file, img, -1 );
+      prepMips( pimg );
+      return pimg;
+    }
+
+    // Copy a specific mip-level from the source image to the atlas
+    Image copyMip( int mipLevel, prImg src, prImg dest ) {
+      Graphics g = Graphics.FromImage( dest.mips[ mipLevel ] );
+      int w = src.mips[ mipLevel ].Width, h = src.mips[ mipLevel ].Height;
+      g.InterpolationMode = _mode;
+      g.DrawImage( src.mips[ mipLevel ], (src.id % _xu) * w, (src.id / _xu) * h );
+      g.Dispose();
+      return src.mips[ mipLevel ];
     }
 
     // Add a processed image file to the atlas
     prImg merge( prImg acc, prImg inst ) {
-      int w = inst.raw.Size.Width, h = inst.raw.Size.Height;
-      _g.DrawImage( inst.raw, (inst.id % _xu) * w, (inst.id / _yu) * h,
-                   new Rectangle( 0, 0, w, h ), GraphicsUnit.Pixel );
+      int mip = 0;
+      //inst.mips.Select(bmp => { return copyMip(mip++, inst, acc); });
+      foreach (Image bmp in inst.mips) { copyMip(mip++, inst, acc); }
       if( Packed != null )
         Packed( this, new ImageEventArgs( inst.id, inst.file ) );
       return acc;
@@ -86,7 +134,8 @@ namespace prc {
     int _yu = 0;    // vert tile count
     int _count = 0; // # of tiles
     Size _texSize;  // texture size in pixels
-    Graphics _g;    // Graphics object for atlas
+    InterpolationMode _mode = InterpolationMode.Bilinear;
+    bool _genMips = true;
 
   }
 
@@ -94,11 +143,21 @@ namespace prc {
   // Internal helper class for images / textures
   class prImg {
     public prImg( string path, Image org, int idx ) {
-      file = path; raw = org; id = idx;
+      mips = new List<Image>();
+      file = path; mips.Add( org  ); id = idx;
     }
-    public void save( string path ) { raw.Save( path ); }
+    public void save( string basePath ) {
+      for( int mip = 0; mip < mips.Count; mip++ ) {
+        saveMips( basePath, mip );
+      }
+    }
+    void saveMips( string basePath, int mipLevel ) {
+      string file = string.Format("{0}{1}.png",
+        basePath, mipLevel > 0 ? "-" + mipLevel.ToString() : "");
+      mips[ mipLevel ].Save( file );
+    }
     public string file { get; set; }
-    public Image raw { get; set; }
+    public List<Image> mips { get; set; }
     public int id { get; set; }
   }
 
